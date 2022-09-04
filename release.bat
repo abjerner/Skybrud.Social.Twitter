6bff309640c079f43bf89004518131dbcf8f5647
Anders Bjerner <abjerner@skybrud.dk>
2022-09-04 16:21:01 +0200

    Misc build stuff

--- release.bat
+++ release.bat
@@ -1,2 +1,2 @@
 @echo off
-dotnet build src/Skybrud.Social.Twitter --configuration Debug /t:rebuild /t:pack -p:BuildTools=1 -p:PackageOutputPath=c:/nuget
+dotnet build src/Skybrud.Social.Twitter --configuration Release /t:rebuild /t:pack -p:BuildTools=1 -p:PackageOutputPath=../../releases/nuget
